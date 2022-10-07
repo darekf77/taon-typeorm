07ed329efda417ea1b0d2519e1807c76d61efb22
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-10-07 16:57:02 +0200

    a

--- src/lib/typeorm/driver/Query.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This class stores query and its parameters
- */
-export class Query {
-    readonly "@instanceof" = Symbol.for("Query")
-
-    constructor(public query: string, public parameters?: any[]) {}
-}
--- src/lib/typeorm/error/QueryRunnerAlreadyReleasedError.ts
+++
@@ -1,7 +0,0 @@
-import { TypeORMError } from "./TypeORMError"
-
-export class QueryRunnerAlreadyReleasedError extends TypeORMError {
-    constructor() {
-        super(`Query runner already released. Cannot run queries anymore.`)
-    }
-}
