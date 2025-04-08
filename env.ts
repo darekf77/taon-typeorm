b7b88078766dbf48433b30936ebab72e9166db5c
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 20:29:40 +0200

    release: v18.0.20

---
+++ env.ts
@@ -0,0 +1,6 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: { domain: 'taon-typeorm.example.domain.com', title: 'Taon Typeorm' },
+};
+export default env;
