ff9fca0c650b9f21b5ae362f830d2d52b29397fa
Dariusz Filipiak <darekf77@gmail.com>
2025-08-01 10:55:06 +0200

    chore: update

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,36 @@
-//#region @notForNpm
-
-//#region @browser
-    import { NgModule } from '@angular/core';
-    import { Component, OnInit } from '@angular/core';
-
-
-    @Component({
-      selector: 'app-firedev-typeorm',
-      template: 'hello from firedev-typeorm'
-    })
-    export class TaonTypeormComponent implements OnInit {
-      constructor() { }
-
-      ngOnInit() { }
-    }
-
-    @NgModule({
-      imports: [],
-      exports: [TaonTypeormComponent],
-      declarations: [TaonTypeormComponent],
-      providers: [],
-    })
-    export class TaonTypeormModule { }
-    //#endregion
-
-    //#region @backend
-    async function start(port: number) {
-      console.log('hello world from backend');
-    }
-
-    export default start;
-
-//#endregion
-
-//#endregion
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+  
