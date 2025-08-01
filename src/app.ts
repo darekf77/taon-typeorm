f75a7bd4d88162b908fbccf503005bbb850b5ca8
Dariusz Filipiak <darekf77@gmail.com>
2025-08-01 12:00:02 +0200

    release: v19.0.30

--- src/app.ts
+++ src/app.ts
@@ -33,4 +33,21 @@
 /* */
 /* */
 /* */
-  
+
+//#region  taon-typeorm component
+//#region @browser
+@Component({ template: 'hello world fromr taon-typeorm' })
+export class TaonTypeormComponent {}
+//#endregion
+//#endregion
+
+//#region  taon-typeorm module
+//#region @browser
+@NgModule({
+  declarations: [TaonTypeormComponent],
+  imports: [CommonModule],
+  exports: [TaonTypeormComponent],
+})
+export class TaonTypeormModule {}
+//#endregion
+//#endregion
