b954abad920ce36efce6f64d9cdb320803d1c549
Dariusz Filipiak <darekf77@gmail.com>
2025-08-03 21:34:22 +0200

    release: v19.0.33

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,7 +1,8 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.taon-typeorm';
+export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'taon-typeorm';
-export const CURRENT_PACKAGE_VERSION = '19.0.32';
+export const CURRENT_PACKAGE_VERSION = '19.0.33';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
